690c077a933c758b9f87a5801e185100fe0bce3a
zhqing007 <543893956@qq.com>
2015-06-08 21:31:04 +0800

    20150608

--- BScripServiceLibrary/Service1.cs
+++ BScripServiceLibrary/Service1.cs
@@ -7,7 +7,7 @@ using System.Text;
 
 namespace BScripServiceLibrary {
     // 注意: 使用“重构”菜单上的“重命名”命令，可以同时更改代码和配置文件中的类名“Service1”。
-    public class Service1 : IService1 {
+    public class BSService : IBSService {
         public string GetData(int value) {
             return string.Format("You entered: {0}", value);
         }
